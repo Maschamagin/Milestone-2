954f235f4242cd783ea93664162621ebcdd3524c
Julian Geger <j_gegner@web.de>
2018-11-27 15:56:07 +0100

    Add files via upload

--- Aufgabe 1/Iterator.cpp
+++ Aufgabe 1/Iterator.cpp
@@ -103,7 +103,7 @@ ElasticNet* Iterator::getNet(){
              for(auto b = nodes.begin(); b != nodes.end(); b++){
                  //iterate through nodes again to calc denominator of vIA
                  //denominator += exp(-(pow((net->getCities()[i] - net->getNodes()[b]).magnitude(),2))/getT());
-                 denominator += exp(-(pow((*a-*b).magnitude(),2))/getT());
+                 denominator += exp(-(pow((*i-*b).magnitude(),2))/getT());
                  //sum(b e nodes) e^((|x_i - y_b|^2)/T)
              }
              vIA = exp(-(pow((*i - *a).magnitude(),2))/getT())/denominator;
@@ -140,17 +140,7 @@ ElasticNet* Iterator::getNet(){
 
      a_counter = 0;
 
-     //at this point all deltaYa are calculated and saved in deltaY
-     //in the next loop all yA get changed by deltaYa
-     //cout << "delta Y : \n";
-//     for(auto a = this->getNet()->getNodes().begin(); a != this->getNet()->getNodes().end(); a++){
-//         *a += deltaY[a_counter];
-//         cout << a->x << " " << a->y << endl;
-//         a_counter++;
-//         //net->changeNet(a_counter, deltaY[a_counter]);
-//     }
          for(int a = 0; a < net->getNumberNodes(); a++){
-             //net->getNodes()[a] += deltaY[a]; ging nicht
              net->changeNet(a, deltaY[a]);
         }
 
@@ -158,69 +148,6 @@ ElasticNet* Iterator::getNet(){
  iterCounter += 1;
 
  }
-
-//void Iterator::apply(){
-
-//    setCurrentTemperature();
-
-//    int numberNodes = net->getNumberNodes();
-//    //set up array to save all deltaY values
-//    Point * deltaY = new Point[numberNodes];
-
-//    double vIA = 0;
-//    double denominator = 0; //used to calc vIA
-//    Point deltaYa = Point();
-
-//    for(int a = 0; a < numberNodes; a++){
-//        //iterate through all elastic net nodes
-
-//        for(int i = 0; i < net->getNumberCities(); i++){
-//            //iterate through cities
-//            for(int b = 0; b < numberNodes; b++){
-//                //iterate through nodes again to calc denominator of vIA
-//                denominator += exp(-(pow((net->getCities()[i] - net->getNodes()[b]).magnitude(),2))/getT());
-//                //sum(b e nodes) e^((|x_i - y_b|^2)/T)
-//            }
-//            vIA = exp(-(pow((net->getCities()[i] - net->getNodes()[a]).magnitude(),2))/getT())/denominator;
-//            //e^((|x_i - y_a|^2)/T) / sum(b e nodes) e^((|x_i - y_b|^2)/T)
-
-//            deltaYa += (net->getCities()[i] - net->getNodes()[a]) * vIA;
-
-//        }
-
-//        deltaYa *= getAlpha();
-
-//        deltaYa += (net->getNodes()[((a-1) + numberNodes)%numberNodes] + net->getNodes()[(a+1)%numberNodes] - (net->getNodes()[a]*2)) * getBeta()*getCurrentTemperature();
-//                                    //c++ doesnt support modulo op. with negative values
-
-//        deltaY[a] = deltaYa;
-
-
-//        //reset
-//        denominator = 0;
-//        deltaYa = Point();
-
-
-//    }
-
-//    //at this point all deltaYa are calculated and saved in deltaY
-//    //in the next loop all yA get changed by deltaYa
-//    //cout << "delta Y : \n";
-//    for(int a = 0; a < net->getNumberNodes(); a++){
-//        //net->getNodes()[a] += deltaY[a]; ging nicht
-//        net->changeNet(a, deltaY[a]);
-
-//        cout << "(" << deltaY[a].x << " " << deltaY[a].y << "), ";
-//        cout << "\n elastic net : \n";
-//        cout << net->getNodes()[a].x << " " << net->getNodes()[a].y << endl ;
-//    }
-
-//    iterCounter += 1;
-
-//}
-
-
-
 
 void Iterator::solve(){
 
